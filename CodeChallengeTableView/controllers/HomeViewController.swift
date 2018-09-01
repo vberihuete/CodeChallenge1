9c5a03978dbb1f359a792ba657e8aca936419ec4
vberihuete <vincentberihuete809@gmail.com>
2018-08-31 21:41:56 -0400

    Adding dummy search bar; adding dummy tab bar items

--- CodeChallengeTableView/controllers/HomeViewController.swift
+++ CodeChallengeTableView/controllers/HomeViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var containerSV: UIStackView!
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var filterView : FilterView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +28,58 @@ class HomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK - Setup
+    func setup(){
+        searchController.isActive = true
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        definesPresentationContext = false
+        
+        filterView = FilterView(items: ["Suggested", "Viewed", "Favorites"], color: .lightGray, selectedColor: UIColor(red: 233/255, green: 16/255, blue: 27/255, alpha: 1), selectedIndex: 0)
+        filterView.delegate = self
+        
+        self.containerSV.insertArrangedSubview(filterView, at: 0)
+        
+        self.containerSV.addConstraint(NSLayoutConstraint(item: self.filterView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40))
     }
-    */
 
+}
+
+
+//MARK: - Search bar
+
+extension HomeViewController: UISearchResultsUpdating{
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        filterData(searchCriteria: searchController.searchBar.text ?? "")
+//        self.tableView.reloadData()
+    }
+    
+    func filterData(searchCriteria: String){
+//        filteredData = data.filter({ (message) -> Bool in
+//            return (message[DATA.MESSAGE.EMAIL] as? String)?.lowercased().contains(searchCriteria.lowercased()) ?? false || (message[DATA.MESSAGE.NAME] as? String)?.lowercased().contains(searchCriteria.lowercased()) ?? false
+//        })
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+}
+
+
+//MARK: - FilterView Delegate
+extension HomeViewController: FilterViewDelegate{
+    
+    func filterView(selected index: Int) {
+//        contentLabel.text = "Selected \(index)"
+    }
+    
 }
